effca7026a7397583f07b34d35808acc187c0f60
Gus-Victrix <iamfrsncis@gmail.com>
2024-02-26 18:14:50 +0300

    :sparkles: feat: deleting state by id is now possible.

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -5,6 +5,7 @@ from api.v1.views import app_views
 from models.state import State
 from models import storage
 
+
 @app_views.route('/states', strict_slashes=False)
 def states():
     """Return a list of all states"""
@@ -13,6 +14,7 @@ def states():
         states.append(state.to_dict())
     return (jsonify(states))
 
+
 @app_views.route('/states/<state_id>', strict_slashes=False)
 def states_id(state_id):
     """Return a state by id"""
@@ -20,3 +22,16 @@ def states_id(state_id):
     if state is None:
         abort(404)
     return (jsonify(state.to_dict()))
+
+
+@app_views.route("/states/<state_id>",
+                 methods=["DELETE"],
+                 strict_slashes=False)
+def rm_state(state_id):
+    """Delete state by id"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    storage.delete(state)
+    storage.save()
+    return (jsonify({}))
