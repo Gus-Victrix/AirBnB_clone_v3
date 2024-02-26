602f8da6c11cbbd9a3f356c386010e4563239ceb
Gus-Victrix <iamfrsncis@gmail.com>
2024-02-26 21:58:47 +0300

    :sparkles: feat: routing amenities by id now

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Amenity objects that handles all default RestFul API actions"""
 
 from api.v1.views import app_views
 from flask import jsonify, request, abort
@@ -12,3 +13,13 @@ def get_amenities():
     amenities = storage.all(Amenity).values()
     amenities = [amenity.to_dict() for amenity in amenities]
     return jsonify(amenities)
+
+
+@app_views.route('/amenities/<amenity_id>',
+                 methods=['GET'], strict_slashes=False)
+def get_amenity(amenity_id):
+    """Retrieves a Amenity object"""
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    return jsonify(amenity.to_dict())
