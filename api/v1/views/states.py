125e4d123261d81b76cb278ba5e6e5a4e548bf6a
Gus-Victrix <iamfrsncis@gmail.com>
2024-02-26 16:51:36 +0300

    :sparkles: feat: /state route and /state/id routes added

---
+++ api/v1/views/states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+from flask import jsonify, redirect, abort
+from api.v1.views import app_views
+from models.state import State
+from models import storage
+
+@app_views.route('/states', strict_slashes=False)
+def states():
+    """Return a list of all states"""
+    states = []
+    for state in storage.all(State).values():
+        states.append(state.to_dict())
+    return (jsonify(states))
+
+@app_views.route('/states/<state_id>', strict_slashes=False)
+def states_id(state_id):
+    """Return a state by id"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    return (jsonify(state.to_dict()))
