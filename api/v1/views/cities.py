0e350ebbcfdd81715a4d949955be37b7f097b4c6
Gus-Victrix <iamfrsncis@gmail.com>
2024-02-26 21:26:24 +0300

    :sparkles: feat: routes for creating and updating cities added

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 from api.v1.views import app_views
-from flask import abort, jsonify
+from flask import abort, jsonify, make_response, request
 from models import storage
 from models.city import City
 from models.state import State
@@ -33,3 +33,33 @@ def rm_city(city_id):
     storage.delete(city)
     storage.save()
     return jsonify({})
+
+@app_views.route("/states/<state_id>/cities",
+                 methods=["POST"], strict_slashes=False)
+def mk_city(state_id):
+    """Creates a City object"""
+    state = storage.get("State", state_id)
+    if state is None:
+        abort(404)
+    if not request.get_json():
+        return make_response(jsonify({"error": "Not a JSON"}), 400)
+    if "name" not in request.get_json():
+        return make_response(jsonify({"error": "Missing name"}), 400)
+    city = City(**request.get_json())
+    city.state_id = state_id
+    city.save()
+    return (make_response(jsonify(city.to_dict()), 201))
+
+@app_views.route("/cities/<city_id>", methods=["PUT"], strict_slashes=False)
+def upd_city(city_id):
+    """Updates a City object"""
+    city = storage.get("City", city_id)
+    if city is None:
+        abort(404)
+    if not request.get_json():
+        return make_response(jsonify({"error": "Not a JSON"}), 400)
+    for key, value in request.get_json().items():
+        if key not in ["id", "state_id", "created_at", "updated_at"]:
+            setattr(city, key, value)
+    city.save()
+    return jsonify(city.to_dict())
